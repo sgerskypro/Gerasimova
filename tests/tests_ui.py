d993ea647b9fde0acf2905879da98ee7f0bdfcad
sgerskypro <sger.skypro@gmail.com>
2025-06-30 19:13:54 +0300

    ДИПЛОМ1!

--- tests/tests_ui.py
+++ tests/tests_ui.py
@@ -3,8 +3,6 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
 from locators.locators import TestData, MainPageLocators, SearchPageLocators, CartPageLocators # Импорт локаторов
 
 
